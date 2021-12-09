c79e652f4f56c312f5b016d544bf5f4b4c3a5060
Mateo <kiiultra19@gmail.com>
2021-12-09 15:25:18 -0500

    complete functions

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,77 +1,23 @@
 package main;
 
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.GeneralSecurityException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.Signature;
-import java.security.SignatureException;
-
-
 import model.Keys;
+import model.Signer;
 
 public class Main {
 	
-	
-	
-	
-	public static void keygenerator() { 
-	
-		try {
-
-			Keys keys = new Keys();
-			keys.keyPairGenerator();
-			keys.exportPrivatekey();
-			keys.exportPublickey();
-			
-		
-			Signature signature = Signature.getInstance("SHA256WithDSA");
-
-			signature.initSign(keys.importPrivateKey(Keys.PRIVATE_KEY_PATH), keys.getSecureRandom());
-			
-			byte[] data = "abcdefghijklmnopqrstuvxyz".getBytes("UTF-8");
-			signature.update(data);
-			
-			byte[] digitalSignature = signature.sign();
-
-
-			Signature signature2 = Signature.getInstance("SHA256WithDSA");
-			signature2.initVerify(keys.importPublicKey(Keys.PUBLIC_KEY_PATH));// aqui se agrega el archivo (proceso) de la llave publica para la opcion 3
-
-			signature2.update(data);// aqui se agrega el archivo original en la opcion 3
-
-			boolean verified = signature2.verify(digitalSignature);// aqui se agrega el archivo firmado para ser verificado en la opcion 3
-
-			System.out.println("verified = " + verified);
-
-			
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SignatureException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (GeneralSecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	}
+	public static final String FILE_PATH = "./prueba.txt";
 
 	public static void main(String[] args) {
-		keygenerator();
+		Keys keys = new Keys();
+		keys.keyPairGenerator();
+		keys.exportPrivatekey();
+		keys.exportPublickey();
+		
+		Signer signer = new Signer();
+//		signer.sifnFile(keys.importPrivateKey(keys.PRIVATE_KEY_PATH), FILE_PATH , keys.getSecureRandom());
+		boolean verified = signer.verifySignature(keys.importPublicKey(Keys.PUBLIC_KEY_PATH), Signer.SIGNATURE_PATH, FILE_PATH);
+		System.out.println("verified = " + verified);
 
 	}
 
--- src/model/Signer.java
+++ src/model/Signer.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Signature;
 import java.security.SignatureException;
@@ -23,6 +24,13 @@ public class Signer {
 	public Signer() {
 	}
 	
+	
+	/***
+	 * 
+	 * @param privateKey
+	 * @param filePath
+	 * @param secureRandom
+	 */
 	public void sifnFile (PrivateKey privateKey, String filePath, SecureRandom secureRandom) {
 		
 		try {
@@ -32,17 +40,8 @@ public class Signer {
 			signature.initSign(privateKey, secureRandom);
 			
 			
-			//transforma el archivo ingresado como ruta a bytes para poder firmarlo
-			File dataFile = new File(filePath);
-			FileInputStream fis = new FileInputStream(dataFile);
-			DataInputStream dis = new DataInputStream(fis);
-			byte[] dataBytes = new byte[(int) dataFile.length()];
-			dis.readFully(dataBytes);
-			dis.close();
-			fis.close();
-			
 			//se elabora la firma basada en los datos del archivo original
-			signature.update(dataBytes);
+			signature.update(getBytesFromFile(filePath));
 			digitalSignature = signature.sign();
 			
 			//se guarda la firma en un archivo tipo .txt
@@ -69,6 +68,63 @@ public class Signer {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	
+	
+	/***
+	 * 
+	 * @param publicKey
+	 * @param signaturePath
+	 * @param filePath
+	 * @return
+	 */
+	public boolean verifySignature(PublicKey publicKey, String signaturePath, String filePath) {
+	
+		try {
+			//toma el archivo que contiene la forma y lee sus datos para guardarlos en la variable  signature
+			byte[] signature = getBytesFromFile(signaturePath); 
+			
+			Signature signature2 = Signature.getInstance("SHA256WithDSA");
+			signature2.initVerify(publicKey);
+			signature2.update(getBytesFromFile(filePath));
+			
+			return signature2.verify(signature);
+		} catch (SignatureException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	
+	public byte[] getBytesFromFile(String path) {
+		try {
+			File dataFile = new File(path);
+			FileInputStream fis;
+			fis = new FileInputStream(dataFile);
+			DataInputStream dis = new DataInputStream(fis);
+			byte[] dataBytes = new byte[(int) dataFile.length()];
+			dis.readFully(dataBytes);
+			dis.close();
+			fis.close();	
+			return dataBytes;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 
 }
